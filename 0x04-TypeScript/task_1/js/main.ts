642da2638230f6d9a3943490d3e88f31b0d23109
Hillary Robert <roberthillary30@gmail.com>
2025-07-13 16:05:50 +0300

    Added Directors as Teacher extenstion

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,12 +8,27 @@ interface Teacher {
 }
 
 const teacher3: Teacher = {
-  firstName: 'John',
+  firstName: 'Hillary',
   fullTimeEmployee: false,
-  lastName: 'Doe',
+  lastName: 'Robert',
   location: 'London',
   contract: false,
 };
 
 
 console.log(teacher3);
+
+
+interface Directors extends Teacher{
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
